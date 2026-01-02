c01422d3a504eb3e1d8d7a260a33eb2505e9d3ac
leorigo1 <leonardoluisorlandorigo@gmail.com>
2026-01-02 14:44:53 -0300

    add services for user

--- Backend-Java/demo/src/main/java/calling/services/UserService.java
+++ Backend-Java/demo/src/main/java/calling/services/UserService.java
@@ -2,6 +2,7 @@ package calling.services;
 
 import org.springframework.stereotype.Service;
 
+import calling.entities.UserEntity;
 import calling.repositories.UserRepository;
 
 @Service
@@ -9,6 +10,17 @@ public class UserService {
 
 	private UserRepository userRepository;
 	
+	public void createUser (UserEntity userEntity) {
+		userRepository.save(userEntity);
+	}
+	
+	public void findUserById (UserEntity userEntity) {
+		userRepository.findById(userEntity.getId());
+	}
+	
+	public void deleteUser (UserEntity userEntity) {
+		userRepository.delete(userEntity);
+	}
 	
 	
 }
