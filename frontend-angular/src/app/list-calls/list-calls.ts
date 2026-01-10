5390ab88f384d8097a38883777b8ea11c8b0de74
leorigo1 <leonardoluisorlandorigo@gmail.com>
2026-01-09 22:50:30 -0300

    add a async func to resolve the conflicts angular and profissionalize codes

--- frontend-angular/src/app/list-calls/list-calls.ts
+++ frontend-angular/src/app/list-calls/list-calls.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChamadoService } from '../services/ChamadoService';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list-calls',
@@ -9,20 +10,11 @@ import { ChamadoService } from '../services/ChamadoService';
   templateUrl: './list-calls.html',
   styleUrl: './list-calls.scss',
 })
-export class ListCalls implements OnInit {
+export class ListCalls {
 
-  chamados: any[] = [];
+chamados$: Observable<any[]>;
 
-  constructor(
-    private service: ChamadoService,
-    private cdr: ChangeDetectorRef
-  ) {}
-
-  ngOnInit(): void {
-    this.service.listar().subscribe(dados => {
-      console.log('DADOS:', dados);
-      this.chamados = dados;
-      this.cdr.detectChanges(); // 🔥 FORÇA renderização
-    });
+  constructor(private service: ChamadoService) {
+    this.chamados$ = this.service.listar();
   }
 }
