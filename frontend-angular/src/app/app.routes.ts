51f4e16b602e4f2f699f71441d9d15b8c67e28c6
unknown <leonardoluisorlandorigo@gmail.com>
2026-01-12 09:35:23 -0300

    add a landing page component and change background color black for white

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { CreateCall } from './create-call/create-call'
 import { ListCalls } from './list-calls/list-calls';
+import { Landing } from './landing/landing';
 
 export const routes: Routes = [
     { path: 'login', component: Login },
     { path: 'iniciar-chamado', component: CreateCall },
-    {path: 'listar-chamados', component: ListCalls},
+    { path: 'listar-chamados', component: ListCalls },
+    { path: '', component: Landing}
 ];
