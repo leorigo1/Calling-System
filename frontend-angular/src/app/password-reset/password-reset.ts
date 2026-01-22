cfc36fb586f7bc4fb02adb4dde2045a4f030e34d
unknown <leonardoluisorlandorigo@gmail.com>
2026-01-22 10:15:06 -0300

    create a page password-reset with metods console.log

--- frontend-angular/src/app/password-reset/password-reset.ts
+++ frontend-angular/src/app/password-reset/password-reset.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-password-reset',
+  standalone: true,
   imports: [],
   templateUrl: './password-reset.html',
   styleUrl: './password-reset.scss',
 })
 export class PasswordReset {
+
+  password = '';
+  passwordconfirm = '';
+
+constructor (private router: Router) {}
+
+  alterar(password: String, passwordconfirm: String) {
+    if (password != passwordconfirm) {
+      alert('As senhas não conferem!')
+      return;
+    }
+    if (password.length < 8 || passwordconfirm.length < 8) {
+      alert('Sua senha deve ter mais que 8 caracteres!')
+      return
+    }
+
+    console.log(password, passwordconfirm)
+    return this.router.navigate(['/iniciar-chamado'])
+  }
+
 
 }
